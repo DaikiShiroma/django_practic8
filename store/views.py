4527b34f13d1ecd9206daf16d473d1b2a7675ac0
DaikiShiroma <daiki20000417@gmail.com>
2023-04-04 21:35:54 +0900

    505エラーハンドリングの実行

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 
 def item_list(request):
     items = Items.objects.all()
-    return render(request, 'store/item_list.html', context={
+    return render(request, 'store/itemlist.html', context={
         'items': items
     })
 
@@ -28,3 +28,6 @@ def one_item(request):
 def page_not_found(request, exception):
     return render(request, 'store/404.html', status=404)
     # return redirect('store:item_list')
+
+def server_error(request):
+    return render(request, 'store/500.html', status=500)
